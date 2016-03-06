86c415fc02fd682e3467bd1b1e31d842762a5547
TomaNikolov <on.tomas@gmail.com>
2016-03-06 14:47:56 +0200

    implemented password hash

--- data/models/User.js
+++ data/models/User.js
@@ -1,17 +1,21 @@
 'use strict';
 
 var mongoose = require('mongoose');
+var encryption = require('../../utils/encryption');
 
 module.exports.init = function () {
     var userSchema = mongoose.Schema({
         username: {type: String, required: true, unique: true},
-        password: {type: String, required: true},
-        accessToken: {type: String}
+        accessToken: {type: String},
+        salt: String,
+        hashPass: {type: String, required: true}
     });
 
-    userSchema.methods.hasValidPassword = function (password) {
-        return password === this.password;
-    };
+    userSchema.method({
+        authenticate: function (password) {
+            return (encryption.generateHashedPassword(this.salt, password) === this.hashPass)
+        }
+    });
 
     mongoose.model('User', userSchema);
 };
